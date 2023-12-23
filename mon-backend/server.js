e006a2ecf20597a7723ad4ccc928b41ce207e642
mileina <meas.pinya@gmail.com>
2023-12-23 23:15:18 +0100

    Suppression des duplicatas de code et utilisation des variables d'environnement pour la DB

--- mon-backend/server.js
+++ mon-backend/server.js
@@ -4,18 +4,18 @@ const cors = require('cors');
 const mysql = require('mysql');
 
 const app = express();
-const port = 3001; 
+const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connection to MariaDB
+// Connection to MariaDB with environment variables
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', 
-  database: 'crushme'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS, 
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
@@ -24,40 +24,16 @@ db.connect((err) => {
 });
 
 // Routes
+
+// Accueil du serveur
 app.get('/', (req, res) => {
   res.send('Server is up and running');
 });
 
-// Fetching invitation data
+// Récupération des données d'invitation
 app.get('/api/invitation/:id', (req, res) => {
     const invitationId = req.params.id;
-    
-    const query = 'SELECT * FROM invitations WHERE id = ?'; 
-
-    db.query(query, [invitationId], (err, result) => {
-        if (err) {
-            res.status(500).send('Error retrieving data');
-        } else {
-            if (result.length > 0) {
-                res.json(result[0]);
-            } else {
-                res.status(404).send('Invitation not found');
-            }
-        }
-    });
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
-
-
-app.get('/api/invitation/:id', (req, res) => {
-    const invitationId = req.params.id;
-    
     const query = 'SELECT * FROM invitations WHERE id = ?';
-
     db.query(query, [invitationId], (err, result) => {
         if (err) {
             res.status(500).json({ error: 'Error retrieving data' });
@@ -71,17 +47,20 @@ app.get('/api/invitation/:id', (req, res) => {
     });
 });
 
-
+// Création d'une nouvelle invitation
 app.post('/api/invitation', (req, res) => {
     const { id, email, date, messageOui, messageNon } = req.body;
-    
     const query = 'INSERT INTO invitations (id, email, date, messageOui, messageNon) VALUES (?, ?, ?, ?, ?)';
     db.query(query, [id, email, date, messageOui, messageNon], (err, result) => {
       if (err) {
-        res.status(500).send('Error saving data');
+        res.status(500).json({ error: 'Error saving data' });
       } else {
         res.json({ message: 'Invitation saved', id });
       }
     });
-  });
-  
+});
+
+// Démarrage du serveur
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
